4db0cd2bb47ee2f3ab0bf1f4bd1a312e6d40cc60
Manuel Ernesto Bonilla Muñnoz <mebonilla9@gmail.com>
2022-10-26 01:08:28 -0500

    Added theme color and icons

--- app/src/main/java/co/edu/umb/mapsapplication/application/ui/theme/Color.kt
+++ app/src/main/java/co/edu/umb/mapsapplication/application/ui/theme/Color.kt
@@ -1,8 +1,24 @@
 package co.edu.umb.mapsapplication.application.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF3A5BA9)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF3A5BA9)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFEFBFF)
+val md_theme_light_onBackground = Color(0xFF001946)
+val md_theme_light_surface = Color(0xFFFEFBFF)
+val md_theme_light_onSurface = Color(0xFF001946)
+
+val md_theme_dark_primary = Color(0xFFB2C5FF)
+val md_theme_dark_onPrimary = Color(0xFF002C72)
+val md_theme_dark_secondary = Color(0xFFB1C5FF)
+val md_theme_dark_onSecondary = Color(0xFF002C72)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_background = Color(0xFF001946)
+val md_theme_dark_onBackground = Color(0xFFDAE2FF)
+val md_theme_dark_surface = Color(0xFF001946)
+val md_theme_dark_onSurface = Color(0xFFDAE2FF)
